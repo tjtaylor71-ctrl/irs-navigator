b55db0d1c542c2eac37dbd575786bed11d14bb9a
tjtaylor71-ctrl <tjtaylor71@gmail.com>
2026-08-24 17:35:59 -0500

    Update server.py

--- server.py
+++ server.py
@@ -37,13 +37,14 @@ import email_service
 from planning.routes import planning_bp
 from mj_routes import mj_bp
 from tdm_routes import tdm_bp
+from mj_scripts_routes import mj_scripts_bp
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
 app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-change-in-production")
 app.register_blueprint(planning_bp, url_prefix='/planning')
 app.register_blueprint(mj_bp)
 app.register_blueprint(tdm_bp)
-
+app.register_blueprint(mj_scripts_bp)
 
 @app.route("/debug-mj-check")
 def debug_mj_check():
